b5370e99a5df910f73b26742b5bf22b6b6427045
bernardobsfonseca <bernardobsfonseca@protonmail.com>
2024-10-19 16:31:56 -0300

    fix: status adjusts

--- Characters/enemies.py
+++ Characters/enemies.py
@@ -1,13 +1,13 @@
 from Characters.enemy import Enemy
 
 goblinoide = Enemy("Goblinoide", 15, 2)
-ogroide = Enemy("Ogroide", 30, 5)
+ogroide = Enemy("Ogroide", 25, 5)
 imp = Enemy("Imp", 10, 6)
 ondine = Enemy("Ondine", 12, 4)
-sylph = Enemy("Sylph", 10, 4)
-gnome = Enemy("Gnome", 20, 3)
+sylph = Enemy("Sylph", 11, 4)
+gnome = Enemy("Gnome", 20, 2)
 fire_salamander = Enemy("Fire Salamander", 12, 6)
-dragon = Enemy("Dragon", 50, 7)
+dragon = Enemy("Dragon", 50, 9)
 
 list_enemies = [
     goblinoide,
--- Misc/global_vars.py
+++ Misc/global_vars.py
@@ -1,3 +1,3 @@
 model_path = "C:/Unesp/TCC/Models/llama-2-7b-chat.Q4_K_M.gguf"
-potion_price = 15
-sharpen_price = 30
+potion_price = 10
+sharpen_price = 20
