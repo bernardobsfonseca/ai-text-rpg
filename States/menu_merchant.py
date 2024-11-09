6084756f1a4e1e1ce7f3e6fecdbd49834bab5560
bernardobsfonseca <bernardobsfonseca@protonmail.com>
2024-11-09 18:47:11 -0300

    fix: functions names

--- States/menu_merchant.py
+++ States/menu_merchant.py
@@ -21,7 +21,7 @@ class MenuMerchant(MenuBase):
 
     def draw_menu(self):
         if not self.scene_loaded:
-            self._initialize_scene()
+            self.initialize_scene()
             misc.clear()
 
         self.draw_market()
@@ -30,27 +30,27 @@ class MenuMerchant(MenuBase):
             print(line)
 
         self.phrase = input("You: ")
-        self.bought = self._process_dialogue(self.phrase)
+        self.bought = self.process_dialogue(self.phrase)
 
         if self.bought is False:
             return self.is_in_merchant
 
-        self._continue_chat()
+        self.continue_chat()
 
         return self.is_in_merchant
 
-    def _initialize_scene(self):
+    def initialize_scene(self):
         misc.draw_line()
         self.start_chat()
-        response = self._get_merchant_response(self.phrase)
+        response = self.get_merchant_response(self.phrase)
         self.draw_market()
-        self._append_chat("Merchant", response['text'])
+        self.append_chat("Merchant", response['text'])
         self.scene_loaded = True
 
-    def _get_merchant_response(self, input_text):
+    def get_merchant_response(self, input_text):
         return self.chat.invoke(input=input_text)
 
-    def _append_chat(self, speaker, text):
+    def append_chat(self, speaker, text):
         print(f'{speaker}: {text}')
         self.chat_list.append(f'{speaker}: {text}')
         self.phrase = input("> ")
@@ -68,16 +68,16 @@ class MenuMerchant(MenuBase):
     def start_chat(self):
         self.chat = self.scene_merchant.create_chain()
 
-    def _process_dialogue(self, phrase):
+    def process_dialogue(self, phrase):
         if 'bye' in phrase.lower():
             self.quit_merchant()
             return False
-        elif self._handle_transaction(phrase):
+        elif self.handle_transaction(phrase):
             return True
         return False
 
-    def _handle_transaction(self, phrase):
-        potions, sharpness, quantity = self._parse_dialogue(phrase)
+    def handle_transaction(self, phrase):
+        potions, sharpness, quantity = self.parse_dialogue(phrase)
 
         if not quantity and not potions and not sharpness:
             return True
@@ -87,19 +87,19 @@ class MenuMerchant(MenuBase):
             return False
 
         if potions:
-            return self._purchase_item('Potion', global_vars.potion_price, quantity, self.hero.buy_potions)
+            return self.purchase_item('Potion', global_vars.potion_price, quantity, self.hero.buy_potions)
         elif sharpness:
-            return self._purchase_item('Sharpen Sword', global_vars.sharpen_price, quantity, self.hero.sharpen_sword)
+            return self.purchase_item('Sharpen Sword', global_vars.sharpen_price, quantity, self.hero.sharpen_sword)
         return True
 
-    def _parse_dialogue(self, phrase):
+    def parse_dialogue(self, phrase):
         potions = re.findall(r'\b(potion|potions)\b', phrase, re.IGNORECASE)
         sharpness = re.findall(r'\b(sharpen|sharpness)\b', phrase, re.IGNORECASE)
         numbers = re.findall(r'\d+', phrase)
         quantity = int(numbers[0]) if numbers else 0
         return potions, sharpness, quantity
 
-    def _purchase_item(self, item_name, price, quantity, purchase_method):
+    def purchase_item(self, item_name, price, quantity, purchase_method):
         total_cost = price * quantity
         if self.hero.gems < total_cost:
             print(f'>>> You don’t have enough money for {quantity} {item_name.lower()}s! <<<')
@@ -122,8 +122,8 @@ class MenuMerchant(MenuBase):
         print(f"POTIONS: {self.hero.potions}")
         print(f"GEMS: {self.hero.gems}")
 
-    def _continue_chat(self):
+    def continue_chat(self):
         self.chat_list.append(f'You: {self.phrase}')
         print('[wait...]')
-        response = self._get_merchant_response(self.phrase)
-        self._append_chat("Merchant", response['text'])
+        response = self.get_merchant_response(self.phrase)
+        self.append_chat("Merchant", response['text'])
